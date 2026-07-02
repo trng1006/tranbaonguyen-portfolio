32fc96f382c99abd9c8040a53003c1b8b9248a1e
Fio <Trng1006.work@gmail.com>
2026-07-02 21:32:09 +0700

    Remove footer completely

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,13 +6,5 @@ import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
-  return (
-    <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Tran Bao Nguyen")}
-        </p>
-      </div>
-    </Fade>
-  );
+  return null;
 }
