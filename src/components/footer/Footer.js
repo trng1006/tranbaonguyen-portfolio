64ba192317e45e52cde26c9cbc2194cfe3c5d10d
Fio <Trng1006.work@gmail.com>
2026-07-03 16:33:04 +0700

    Clean up Footer.js to fix eslint warnings

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,3 @@
-import React, {useContext} from "react";
-import "./Footer.scss";
-import {Fade} from "react-reveal";
-import emoji from "react-easy-emoji";
-import StyleContext from "../../contexts/StyleContext";
-
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
   return null;
 }
